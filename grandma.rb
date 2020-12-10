b184fb98268d86ada81ff10dcad705775a677f2c
Romano Livi <romano_livi@outlook.com>
2020-12-10 22:50:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,10 @@
 def speak_to_grandma(phrase)
-  if phrase.!upcase
-    return "HUH? SPEAK UP, SONNY!"
-  elsif phrase == "I LOVE YOU GRANDMA!"
+  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else
+  elsif phrase.upcase 
     return "NO, NOT SINCE 1938!"
-  end
+  else 
+    return "HUH?! SPEAK UP SONNY!"
 end
 
 
